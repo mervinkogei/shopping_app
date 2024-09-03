ef569174c1e09e644871ea23cf80e6d984c576d4
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-09-03 21:41:21 +0300

    feat: Added a Colors pallette utils

---
+++ lib/Utils/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff5A57FF);
+  static const Color accentColor = Color(0xffFF5789);
+  static const Color secondaryColor = Color(0xffC2C0FF);
+  static const Color primaryLightColor = Color(0xffFAF9FF);
+  static const Color redDanger = Color(0xFFF04248);
+  static const Color warning = Color(0xFFFFD21F);
+  static const Color greenOk =  Color(0xff03B31F);
+  static const Color infoColor =  Color(0xff1C86EE);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color blackColor = Color(0xff353535);
+  static const Color greyColor = Color(0xffCCCCCC);
+  static Color borderColor = const Color(0xff4A4A5F).withOpacity(0.13);
+  static Color textColorColor = const Color(0xff414141);
+  static Color fadedPrimaryColor = const Color(0xff6663FF).withOpacity(0.97);
+  static Color textLightColor = const Color(0xffA3AED0);
+}
