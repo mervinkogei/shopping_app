7eeaf0bc5f5211dd1c1223faa7a186a629f150d1
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-09-04 09:34:57 +0300

    feat : Added the product model

---
+++ lib/Models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String name;
+  final double basePrice;
+  final List<String> sizes;
+  final List<String> colors;
+
+  Product({
+    required this.name,
+    required this.basePrice,
+    required this.sizes,
+    required this.colors,
+  });
+}
